2136ecdfbc3f7885dbedeaa91282e6c5f637ec97
asadmansr <asadmansr@gmail.com>
2017-11-22 20:10:10 -0500

    Initial commit

---
+++ app/src/main/java/com/spotapp/spot/PickParkingActivity.java
@@ -0,0 +1,13 @@
+package com.spotapp.spot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickParkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_parking);
+    }
+}
